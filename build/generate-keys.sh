4836b9d0a2be42eedd726387b4af21297bc5ee89
Ahmed <ahmedshaikx@gmail.com>
2022-04-11 02:58:56 -0400

    first commit

--- build/generate-keys.sh
+++ build/generate-keys.sh
@@ -1,9 +1,9 @@
-#!/bin/zsh
+#!/bin/sh
 
 echo "Generating certificates for use with CircleCI, press enter to continue"
 read check1
-openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
-openssl rsa -passin pass:x -in server.pass.key -out server.key
+openssl genrsa -des3 -passout pass:gsahdg  -out server.pass.key 2048
+openssl rsa -passin pass:gsahdg x -in server.pass.key -out server.key
 rm server.pass.key
 echo "We will now generate the server key, when promoted for a password, press enter"
 echo "press enter to continue"
